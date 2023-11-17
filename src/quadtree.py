9f85e5b643ff561f9b475871fa9239c0ecdf1378
Hugo <94971399+Hogooo@users.noreply.github.com>
2023-11-17 12:03:56 +0100

    Update quadtree.py

--- src/quadtree.py
+++ src/quadtree.py
@@ -1,67 +1,54 @@
 from __future__ import annotations
 from tkinter import *
 
+WIDTH = HEIGHT = 256
+node_posx1 = [0, 1, 0, 1]
+node_posy1 = [0, 0, 1, 1]
+node_posx2 = [1, 2, 1, 2]
+node_posy2 = [1, 1, 2, 2]
+
+
+def get_list_from_string(mainstring) -> list:
+    node_list = []
+    for i in range(4):
+        node_list[i] = eval(mainstring)
+    return node_list
+
+
+def value_is_list(value: int | list) -> bool:
+    if type(value) is list:
+        return True
+
+
+def create_tk_window(quadtree):
+    tk_window = Tk()
+    tk_canvas = Canvas(tk_window, width=(WIDTH * 2), height=(HEIGHT * 2))
+    for i in range(len(quadtree.node_list)):
+        print("type = ", type(first_quadtree.node_list[i]), ", i = ", i)
+        print("x1 = ", node_posx1[i] * WIDTH,
+              "y1 = ", node_posy1[i] * HEIGHT,
+              "x2 = ", node_posx2[i] * WIDTH,
+              "y2 = ", node_posy2[i] * HEIGHT)
+
+        fill = "black" if first_quadtree.node_list[i] else "white"
+        tk_canvas.create_rectangle(node_posx1[i] * WIDTH,
+                                   node_posy1[i] * HEIGHT,
+                                   node_posx2[i] * WIDTH,
+                                   node_posy2[i] * HEIGHT, fill=fill)
+    tk_canvas.pack()
+    tk_window.mainloop()
+
 
 class QuadTree:
-    node_posx1 = [0, 1, 0, 1]
-    node_posy1 = [0, 0, 1, 1]
-    node_posx2 = [1, 2, 1, 2]
-    node_posy2 = [1, 1, 2, 2]
 
-    def __init__(self,
-                 hg: int | QuadTree,
-                 hd: int | QuadTree,
-                 bg: int | QuadTree,
-                 bd: int | QuadTree):
-        self.HEIGHT = self.WIDTH = 256
-        self.tk_window = None
+    def __init__(self, hg: int | list, hd: int | list, bg: int | list, bd: int | list):
         self.node_list = [hg, hd, bg, bd]
         self.depth = 0
-        self.create_tk_window()
-        self.create_tk_canvas()
-
-        for i in range(4):
-            print("self.node_list[i] = ", self.node_list[i], ", i = ", i)
-            if self.value_is_list(self.node_list[i]):
-                print(self.get_sub_list(self.get_list_from_string(self.node_list)))
-
-            else:
-                fill = "black" if self.node_list[i] else "grey"
-                self.tk_window.canvas.create_rectangle(self.node_posx1[i] * self.WIDTH,
-                                                       self.node_posy1[i] * self.HEIGHT,
-                                                       self.node_posx2[i] * self.WIDTH,
-                                                       self.node_posy2[i] * self.HEIGHT,
-                                                       fill=fill)
-
-        self.tk_window.canvas.pack()
-        self.tk_window.mainloop()
-
-    def get_list_from_string(self, value) -> list:
-        for i in range(4):
-            self.node_list[i] = eval(value)
-        return self.node_list
-
-    @staticmethod
-    def get_sub_list(mainlist: list, sublist=None):
-        for i in range(4):
-            sublist[i] = mainlist[i]
-        return sublist
-
-    @staticmethod
-    def value_is_list(value: int | list) -> bool:
-        if type(value) is list:
-            return True
-
-    def create_tk_window(self):
-        self.tk_window = Tk()
-        return self.tk_window
-
-    def create_tk_canvas(self):
-        self.tk_window.canvas = Canvas(self.tk_window, width=self.WIDTH, height=self.HEIGHT)
-        return self.tk_window.canvas
 
 
-first_quadtree = QuadTree(1, 0, 1, 0)
+first_quadtree = QuadTree(1, 0, 0, 1)
+create_tk_window(first_quadtree,)
+# [1, 0, 0, 1]
 
 # "../files/quadtree_easy.txt"
 # "../files/quadtree.txt"
